aed3f92cc04fe9939df348cf521bda5b4b84cb61
Dwayne <dwaynecharrington@gmail.com>
2022-07-21 16:23:17 +1000

    fix(app): new taxonomy and load env vars

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,21 +1,14 @@
-import Aurelia/*, { StyleConfiguration }*/ from 'aurelia';
+import Aurelia, { StyleConfiguration } from 'aurelia';
 import { RouterConfiguration } from '@aurelia/router';
 import { MyApp } from './my-app';
-// Css files imported in this main file are NOT processed by style-loader
-// They are for sharedStyles in shadowDOM.
-// However, css files imported in other js/ts files are processed by style-loader.
-// import shared from './shared.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import shared from './shared.css';
 
 Aurelia
-  /*
   .register(StyleConfiguration.shadowDOM({
-    // optionally add the shared styles for all components
     sharedStyles: [shared]
   }))
-  */
-  .register(RouterConfiguration)
-  // To use HTML5 pushState routes, replace previous line with the following
-  // customized router config.
-  // .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
+  .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
   .app(MyApp)
   .start();
--- wp-config.php
+++ wp-config.php
@@ -3,6 +3,9 @@ if(file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USERNAME'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
