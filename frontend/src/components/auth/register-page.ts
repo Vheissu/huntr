68a833fe262c0adcfa0ba6c7790cd9373bb1b535
Dwayne Charrington <dwaynecharrington@gmail.com>
2022-07-23 23:06:37 +1000

    feat(components): login and register components working

--- frontend/src/components/auth/register-page.ts
+++ frontend/src/components/auth/register-page.ts
@@ -1,4 +1,5 @@
 import { wait } from './../../helpers';
+import { ToastMessage } from './../../services/toast-service';
 import { newInstanceForScope } from '@aurelia/kernel';
 import { IValidationRules } from '@aurelia/validation';
 import { IValidationController } from '@aurelia/validation-html';
@@ -6,11 +7,11 @@ import { IRouter } from '@aurelia/router';
 
 import { IApi } from '../../services/api';
 import { IAuth } from '../../services/auth';
-import { IToastService, ToastMessage } from '../../services/toast-service';
+import { IToastService } from '../../services/toast-service';
 
-export class LoginPage {
-    private showLogin = true;
+export class RegisterPage {
     private email: string;
+    private username: string;
     private password: string;
 
     constructor(
@@ -22,22 +23,23 @@ export class LoginPage {
         @IRouter readonly router: IRouter
     ) {
         validationRules.on(this)
+            .ensure('username').required()
             .ensure('email').required()
-            .ensure('password').required().when(() => this.showLogin);
+            .ensure('password').required();
     }
 
     async handleForm() {
         const result = await this.validationController.validate();
 
         if (result.valid) {
-            const login = await this.auth.login(this.email, this.password);
+            const register = await this.auth.register(this.email, this.username, this.password);
 
-            if (login.success) {
-                localStorage.setItem('token', login.data.jwt);
+            if (register.jwt) {
+                localStorage.setItem('token', register.jwt);
 
-                this.toastService.success(new ToastMessage('You are now logged in. Welcome back!', 'Successfully logged in!'));
+                this.toastService.success(new ToastMessage('Your account has been created. Logging you in...', 'Successfully registered!'));
 
-                await wait(1500);
+                await(wait(1500));
 
                 this.router.load('/');
             }
