9e82f920afd2f91822e5fe2bd969fff9d760d5ed
Dwayne <dwaynecharrington@gmail.com>
2022-07-26 12:53:21 +1000

    chore(frontend): move routes out

--- frontend/src/my-app.ts
+++ frontend/src/my-app.ts
@@ -1,70 +1,72 @@
 import { IRouteableComponent, IRoute } from "@aurelia/router";
 
+const AppRoutes: IRoute[] = [
+    {
+        component: () => import('./components/home/home-page'),
+        path: '',
+        title: 'Home',
+        redirectTo: '/home'
+    },
+    {
+        component: () => import('./components/home/home-page'),
+        path: 'home',
+        title: 'Home',
+    },
+    {
+        id: 'about',
+        component: import('./components/about/about-page'),
+        path: 'about',
+        title: 'About'
+    },
+    {
+        id: 'products',
+        component: () => import('./components/products/products-page'),
+        path: 'products/:product',
+        title: 'Product'
+    },
+    {
+        id: 'topics',
+        component: () => import('./components/topics/topics-page'),
+        path: ['topics', 'topics/:topic'],
+        title: 'Topics'
+    },
+    {
+        id: 'topics',
+        component: () => import('./components/collections/collections-page'),
+        path: ['collections', 'collections/:topic'],
+        title: 'Collections'
+    },
+    {
+        id: 'login',
+        component: () => import('./components/auth/login-page'),
+        path: 'login',
+        title: 'Login',
+        data: {
+            public: true
+        }
+    },
+    {
+        id: 'register',
+        component: () => import('./components/auth/register-page'),
+        path: 'register',
+        title: 'Register',
+        data: {
+            public: true
+        }
+    },
+    {
+        id: 'my-profile',
+        component: () => import('./components/profile/profile-page'),
+        path: 'my/profile',
+        title: 'Profile',
+        data: {
+            isAuth: true
+        }
+    },
+];
+
 export class MyApp implements IRouteableComponent {
 
-    static routes: IRoute[] = [
-        {
-            component: () => import('./components/home/home-page'),
-            path: '',
-            title: 'Home',
-            redirectTo: '/home'
-        },
-        {
-            component: () => import('./components/home/home-page'),
-            path: 'home',
-            title: 'Home',
-        },
-        {
-            id: 'about',
-            component: import('./components/about/about-page'),
-            path: 'about',
-            title: 'About'
-        },
-        {
-            id: 'products',
-            component: () => import('./components/products/products-page'),
-            path: 'products/:product',
-            title: 'Product'
-        },
-        {
-            id: 'topics',
-            component: () => import('./components/topics/topics-page'),
-            path: ['topics', 'topics/:topic'],
-            title: 'Topics'
-        },
-        {
-            id: 'topics',
-            component: () => import('./components/collections/collections-page'),
-            path: ['collections', 'collections/:topic'],
-            title: 'Collections'
-        },
-        {
-            id: 'login',
-            component: () => import('./components/auth/login-page'),
-            path: 'login',
-            title: 'Login',
-            data: {
-                public: true
-            }
-        },
-        {
-            id: 'register',
-            component: () => import('./components/auth/register-page'),
-            path: 'register',
-            title: 'Register',
-            data: {
-                public: true
-            }
-        },
-        {
-            id: 'my-profile',
-            component: () => import('./components/profile/profile-page'),
-            path: 'my/profile',
-            title: 'Profile',
-            data: {
-                isAuth: true
-            }
-        },
-    ];
+    static routes: IRoute[] = AppRoutes;
 
 }
