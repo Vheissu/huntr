c7be69781410af8726a89622500e7c2bf9db4f65
Dwayne <dwaynecharrington@gmail.com>
2022-08-03 15:39:34 +1000

    feat(app): disable vote button if not logged in

--- frontend/src/components/products/products-page.ts
+++ frontend/src/components/products/products-page.ts
@@ -1,11 +1,12 @@
 import { IRouteableComponent } from '@aurelia/router';
 
+import { IAuth } from '../../services/auth';
 import { IApi } from './../../services/api';
 
 export class ProductsPage implements IRouteableComponent {
     private product = null;
 
-    constructor(@IApi readonly api: IApi) {}
+    constructor(@IApi readonly api: IApi, @IAuth readonly auth: IAuth) {}
 
     async load(params) {
         this.product = await this.api.getProduct(params.product);
