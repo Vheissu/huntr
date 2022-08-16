ce96957f26ab65e3afa413dffad13e9d914e5b80
Dwayne <dwaynecharrington@gmail.com>
2022-08-16 15:58:14 +1000

    feat(app): profile page functionality and layout

--- frontend/src/components/profile/profile-page.ts
+++ frontend/src/components/profile/profile-page.ts
@@ -1,3 +1,21 @@
+import { IAuth } from "../../services/auth";
+import { ILoader } from "../../services/loader";
+
 export class ProfilePage {
-    
+    private user = null;
+
+    constructor(@IAuth readonly auth: IAuth, @ILoader readonly loader: ILoader) {
+
+    }
+
+    created() {
+        this.loader.show();
+    }
+
+    async binding() {
+        this.user = await this.auth.user();
+
+        this.loader.hide();
+    }
+
 }
