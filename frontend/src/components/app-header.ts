148f08f024cddada8b83e579629a3f59a3e54e91
Dwayne <dwaynecharrington@gmail.com>
2022-08-23 14:25:42 +1000

    fix(app): hide dropdowns on navigation with router

--- frontend/src/components/app-header.ts
+++ frontend/src/components/app-header.ts
@@ -1,7 +1,7 @@
 import { IAuth } from "../services/auth";
 
 import { Dropdown } from "bootstrap";
-import { INode } from "aurelia";
+import { IEventAggregator, INode } from "aurelia";
 
 export class AppHeader {
   private productDropdown;
@@ -10,9 +10,17 @@ export class AppHeader {
 
   constructor(
     @IAuth readonly auth: IAuth,
-    @INode readonly element: HTMLElement
+    @INode readonly element: HTMLElement,
+    @IEventAggregator readonly ea: IEventAggregator
   ) {
     this.avatar = auth?.token?.avatar_image ?? "";
+  }
+
+  bound() {
+    this.ea.subscribe('au:router:navigation-start', payload => {
+        this.productDropdown.hide();
+        this.profileDropdown.hide();
+    });
   }
 
   attached() {
@@ -26,5 +34,12 @@ export class AppHeader {
 
     this.productDropdown = new Dropdown(dropdownProductElement);
     this.profileDropdown = new Dropdown(dropdownProfileElement);
+
+    // this.element.shadowRoot.addEventListener('click', event => {
+    //     if ((event.target as HTMLElement).tagName !== 'A') {
+    //         this.productDropdown.hide();
+    //         this.profileDropdown.hide();
+    //     }
+    // });
   }
 }
