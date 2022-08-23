91a61a7465c57d62e089b445fdaaa8204a02ef7c
Dwayne <dwaynecharrington@gmail.com>
2022-08-23 14:10:03 +1000

    feat(app): dropdown cleanup and fixes, get dropdown js working

--- frontend/src/components/app-header.ts
+++ frontend/src/components/app-header.ts
@@ -1,9 +1,30 @@
-import { IAuth } from '../services/auth';
+import { IAuth } from "../services/auth";
+
+import { Dropdown } from "bootstrap";
+import { INode } from "aurelia";
 
 export class AppHeader {
-    private avatar = '';
+  private productDropdown;
+  private profileDropdown;
+  private avatar = "";
 
-    constructor(@IAuth readonly auth: IAuth) {
-        this.avatar = auth?.token?.avatar_image ?? '';
-    }
+  constructor(
+    @IAuth readonly auth: IAuth,
+    @INode readonly element: HTMLElement
+  ) {
+    this.avatar = auth?.token?.avatar_image ?? "";
+  }
+
+  attached() {
+    const dropdownProductElement = this.element.shadowRoot.querySelector(
+      ".dropdown-toggle-product"
+    );
+
+    const dropdownProfileElement = this.element.shadowRoot.querySelector(
+      ".dropdown-toggle-profile"
+    );
+
+    this.productDropdown = new Dropdown(dropdownProductElement);
+    this.profileDropdown = new Dropdown(dropdownProfileElement);
+  }
 }
